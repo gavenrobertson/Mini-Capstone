3e956304413ed6365457eca39fd581626c0336f5
Gaven Robertson <gaven.robertson@gmail.com>
2025-08-05 10:45:02 -0500

    added teacher.csv, fortythree92.csv for testing and made some logic changes in main.py

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ def main():
             student_view = student_window()
 
             while True:
-                event, fortythree92_values = student_view.read()
+                event, stu_values = student_view.read()
                 if event in (sg.WINDOW_CLOSED, 'Exit'):
                     student_view.close()
                     break
@@ -39,22 +39,52 @@ def main():
                 elif event == 'Submit':
                     more = stu_values['-MORE-']
                     plus = stu_values['-PLUS-']
+                    last_first = stu_values['-LASTFIRST-']
+                    phone = stu_values['-PHONE-']
+                    room = stu_values['-ROOM-']
 
+                    # Transportation radio buttons
+                    if stu_values['-POV-']:
+                        transportation = 'POV'
+                    elif stu_values['-AIRPLANE-']:
+                        transportation = 'Airplane'
+                    elif stu_values['-BUS-']:
+                        transportation = 'Bus'
+                    elif stu_values['-TRAIN-']:
+                        transportation = 'Train'
+                    elif stu_values['-OTHER-']:
+                        transportation = 'Other'
+                    else:
+                        transportation = None
+
+                    departure_date = stu_values['-DEPARTURE_DATE-']
+                    arrival_one = stu_values['-ARRIVAL_ONE-']
+                    lor_one = stu_values['-LOR_ONE-']
+                    mileage_one = stu_values['-MILEAGE_ONE-']
+                    date_two = stu_values['-DATE_TWO-']
+                    lor_two = stu_values['-LOR_TWO-']
+                    other_name = stu_values['-OTHER_NAME-']
+                    address = stu_values['-ADDRESS-']
+                    other_number = stu_values['-OTHER_NUMBER-']
+                    confirmation_number = stu_values['-CONFIRMATION_NUMBER-']
+
+                    # Lodging type radio buttons
+                    if stu_values['-HOTEL_RENTAL-']:
+                        lodging_type = 'Hotel/Rental'
+                    elif stu_values['-RESIDENCE-']:
+                        lodging_type = 'Residence'
+                    elif stu_values['-CAMP_SITE-']:
+                        lodging_type = 'Camp Site'
+                    else:
+                        lodging_type = None
+
+                    visit_name_one = stu_values['-VISIT_NAME_ONE-']
+                    visit_number_one = stu_values['-VISIT_NUMBER_ONE-']
+                    visit_relationship_one = stu_values['-VISIT_RELATIONSHIP_ONE-']
+                    visit_name_two = stu_values['-VISIT_NAME_TWO-']
+                    visit_number_two = stu_values['-VISIT_NUMBER_TWO-']
+                    visit_relationship_two = stu_values['-VISIT_RELATIONSHIP_TWO-']
                     
-                    student = Student(
-                        dod_id="S789012",
-                        name="Student Name",
-                        rank="Trainee",
-                        email="student@example.com",
-                        squad=plus,
-                        phonenumber=more,
-                        instructor=instructor
-                    )
-
-                    instructor.add_stu(student)
-
-                    # Debug print
-                    print(f"Instructor: {instructor.name}, Student: {student.phonenumber}, {student.squad}")
                     
                     student_view.close()
                     break
